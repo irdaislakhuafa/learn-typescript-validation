e256bd634e8f0b62ef002f54e121996e26ea0c97
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-06-03 00:13:39 +0700

    feat: add auto coercion for z.coerce
    Adds auto coercion for z.coerce to convert data types automatically.
    
    This allows users to pass in any type of data to z.coerce and it will automatically convert it to the correct type. This makes it easier to use z.coerce to validate data and convert it to the correct type.

--- tests/validation.test.ts
+++ tests/validation.test.ts
@@ -19,4 +19,20 @@ describe("zod", () => {
 		expect(user.isAdmin.parse(true)).toBe(true)
 		expect(user.balance.parse(10_000)).toBe(10_000)
 	})
+
+	it("test auto conversion data type", () => {
+		const user = {
+			name: z.coerce.string().min(3).max(10),
+			isAdmin: z.coerce.boolean(),
+			balance: z.coerce.number().min(100).max(10_000_000),
+		}
+
+		expect(user.name.parse(123)).toBe("123")
+		expect(user.name.parse(true)).toBe("true")
+		expect(user.isAdmin.parse("123")).toBe(true)
+		expect(user.isAdmin.parse(null)).toBe(false)
+		expect(user.isAdmin.parse("true")).toBe(true)
+		expect(user.balance.parse(100)).toBe(100)
+		expect(user.balance.parse("100000")).toBe(100_000)
+	})
 })
